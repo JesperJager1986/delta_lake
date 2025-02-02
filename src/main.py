ef15b62448e819b41c853e23cf48cab1a404540c
jesperthoftillemannjaeger <Jesperjag86@gmail.com>
2025-02-02 09:30:09 +0100

    done

--- src/main.py
+++
@@ -1,9 +0,0 @@
-
-from AI_utility.src.download.FileDownLoader import FileDownloader
-
-if __name__ == '__main__':
-    url = "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_mm_gl.csv"
-
-    loader = FileDownloader(url)
-
-    print(2)

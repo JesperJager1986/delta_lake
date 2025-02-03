504e93fdf1127a328b115d2c8f1800cffbe44c06
jesperthoftillemannjaeger <Jesperjag86@gmail.com>
2025-02-03 10:38:36 +0100

    working

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
+from dataclasses import dataclass
+
 from deltalake import DeltaTable
 from pyspark.sql import DataFrame
 import os
 from src.ai_utility.src.download.FileDownLoader import FileDownloader
+from src.ai_utility.src.data_from_modules.y_finance import DataFromYFinance
+
 from pathlib import Path
-from src.spark.spark_handler import Sparkhandler
+from src.spark.spark_handler import SparkHandler
 
 
 
@@ -11,46 +15,28 @@ if __name__ == '__main__':
     #http: // localhost: 4040 / jobs /
 
     #url = "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_mm_gl.csv"
-    url = "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_daily_mlo.csv"
+    #url = "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_daily_mlo.csv"
 
-    filedownloader = FileDownloader(url)
-    store_path = filedownloader.store_path
+    #filedownloader = FileDownloader(url)
+    #store_path = filedownloader.store_path
+
+    yfinance = DataFromYFinance("AAPL")
+    store_path = yfinance.store_file
+
     delta_table_path = Path("/Users/jesperthoftillemannjaeger/PycharmProjects/delta_lake/delta_table") / store_path.stem
 
-    sparkHandler = Sparkhandler("MyApp")
-    spark = sparkHandler.build()
-
-    sample_data = df_data = spark.read.option("comment", "#") \
-    .option("inferSchema", "true") \
-    .option("header", "true") \
-    .csv(str(store_path)) \
-    .limit(5)
-
-    first_row = sample_data.collect()[0] if sample_data.count() > 0 is not None else None
-
-    if first_row is not None:
-        if all(isinstance(first_row, str) for first_row in first_row):
-            header = "true"
-        else:
-            header = "false"
-
-        df_data = spark.read.option("comment", "#") \
-            .option("inferSchema", "true") \
-            .option("header", header) \
-            .csv(str(store_path)) \
-
-    new_data_df: DataFrame = sparkHandler.read_csv_to_dataframe(csv_path=str(store_path) )
-
+    sparkHandler = SparkHandler("MyApp", store_path, "unknown")
+    new_data_sdf = sparkHandler.run()
     if not os.path.exists(delta_table_path):
         print("Delta table does not exist. Creating new Delta table.")
         #os.makedirs(delta_table_path, exist_ok=True)
-        new_data_df.write.format("delta").mode("overwrite").save(str(delta_table_path))
+        new_data_sdf.write.format("delta").mode("overwrite").save(str(delta_table_path))
     else:
-        existing_data_df = spark.read.format("delta").load(str(delta_table_path))
+        existing_data_df = sparkHandler.load_delta_table(str(delta_table_path))
 
-        unique_cols = ["year", "month"]
+        unique_cols = yfinance.unique_columns
 
-        new_records_df = new_data_df.join(existing_data_df, on=unique_cols, how="left_anti")
+        new_records_df = new_data_sdf.join(existing_data_df, on=unique_cols, how="left_anti")
 
         if new_records_df.count() > 0:
             new_records_df.write.format("delta").mode("append").save(delta_table_path)
@@ -64,5 +50,5 @@ if __name__ == '__main__':
 
     history_df = dt.history()  # This will return a DataFrame with the history
     new_data_df = dt.to_pandas()
-    print(new_data_df)
+    print(new_data_sdf)
     print("Done")
--- src/spark/spark_handler.py
+++ src/spark/spark_handler.py
@@ -5,17 +5,17 @@ from pyspark.sql import DataFrame
 from deltalake import DeltaTable
 
 
-class Sparkhandler:
-    def __init__(self, name: str):
+class SparkHandler:
+    def __init__(self, name: str, store_path, header):
         self.name = name
+        self.store_path: str = store_path
+        self.header: str = header
         self.spark = self.build()
-
 
     def build(self):
         builder = pyspark.sql.SparkSession.builder.appName(self.name) \
             .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
             .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-
         return configure_spark_with_delta_pip(builder).getOrCreate()
 
     def read_csv_to_dataframe(self, csv_path: str) -> DataFrame:
@@ -24,7 +24,7 @@ class Sparkhandler:
         :param csv_path: Path to the CSV file.
         :return: Spark DataFrame containing the CSV data.
         """
-        return self.spark.read.option("comment", "#").csv(csv_path, header=True, inferSchema="true")
+        return self.spark.read.format("delta").option("comment", "#").csv(csv_path, header=True, inferSchema="true")
 
     @staticmethod
     def write_to_delta(df: DataFrame, delta_table_path: str) -> None:
@@ -37,7 +37,7 @@ class Sparkhandler:
         print(f"Delta table created at {delta_table_path}")
 
     @staticmethod
-    def delta_to_pandas(self, delta_table_path: str) -> pd.DataFrame:
+    def delta_to_pandas(delta_table_path: str) -> pd.DataFrame:
         """
         Converts a Delta table to a Pandas DataFrame.
         :param delta_table_path: Path to the Delta table.
@@ -45,6 +45,43 @@ class Sparkhandler:
         """
         dt = DeltaTable(delta_table_path)
         return dt.to_pandas()
+
+
+    def find_header(self):
+        sample_data = self.spark.read.option("comment", "#") \
+            .option("inferSchema", "true") \
+            .option("header", "false") \
+            .csv(str(self.store_path)) \
+            .limit(5)
+        first_row = sample_data.collect()[0] if sample_data.count() > 0 is not None else None
+
+        if first_row is not None:
+            if all(isinstance(first_row, str) for first_row in first_row):
+                header = "true"
+            else:
+                header = "false"
+        else:
+            raise Exception("There doesn't seem to be any data")
+
+        return header
+
+    def run(self):
+        if self.header == "unknown":
+            header = self.find_header()
+        else:
+            header = self.header
+
+        df_data = self.spark.read.format("delta").option("comment", "#") \
+                .option("inferSchema", "true") \
+                .option("header", header) \
+                .csv(str(self.store_path))
+        return df_data
+
+
+    def load_delta_table(self, delta_table_path: str) -> DataFrame:
+        sdf = self.spark.read.format("delta").load(str(delta_table_path))
+        return sdf
+
 
     def into(self):
         pass
